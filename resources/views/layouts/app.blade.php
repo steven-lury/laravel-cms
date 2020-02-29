4ad7242df18dafbeaf23cb986e4cc2731e4c7c6b
reza <reza67asgari@gmail.com>
2020-02-29 16:37:25 +0330

    create post model with migration

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title') - {{config('app.name', 'Reza')}}</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    @stack('css')
+</head>
+<body>
+    @include('layouts.header')
+
+    @yield('content')
+
+    @include('layouts.footer')
